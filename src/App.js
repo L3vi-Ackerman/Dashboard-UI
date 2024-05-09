0200cc38f65f8d96d08480663e8e05508674d4b1
l3vi-Ackerman <l3vi3211@gmail.com>
2024-05-09 21:44:02 +0545

    updating App.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { NavBar } from "./Components/Navbar";
+
 import { Home } from "./Pages/HomePage";
 import './App.css'
-import CarouselsSlider from "./Components/Slider";
-import { carousels } from "./data";
 export default function HomePage() {
   return (
     <>
       <Home />
-      {/* <CarouselsSlider /> */}
     </>
   );
 }
